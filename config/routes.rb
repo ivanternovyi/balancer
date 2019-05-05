1bf27b55a5832dfa9bd1f0b49598c78c29391368
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-05-06 00:27:56 +0300

    add tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'home#index'
+  root to: 'tasks#index'
+
+  resources :tasks, only: %i[new create]
 
   authenticate :user do
     mount Sidekiq::Web => '/sidekiq'
