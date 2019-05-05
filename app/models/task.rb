ee2b6bb2bc1a337a19b79350a167bee57a0e79e8
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-05-06 00:18:55 +0300

    factorization methods

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,17 @@
+require 'prime'
+
 class Task < ApplicationRecord
   validates :digit, presence: true
+
+  def compute_factorization
+    update_column(:result, prime_factorization.to_s)
+  end
+
+  private
+
+  def prime_factorization
+    Prime.prime_division(digit).flat_map do |factor, power|
+      [factor] * power
+    end
+  end
 end
