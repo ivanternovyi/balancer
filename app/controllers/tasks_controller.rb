edbd96c67eeb9fedc5a32c24e8db75004149a184
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-05-06 01:52:10 +0300

    index page for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,22 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    @task.compute_factorization
+    redirect_to root_path
+  end
+
+  private
+
+  def task_params
+    params.permit(:digit)
   end
 end
